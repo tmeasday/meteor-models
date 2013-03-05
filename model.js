7f7696be2d4888d82ee382e52aba0836d9a93838
Tom Coleman <tom@thesnail.org>
2013-03-05 11:54:00 +1100

    Started on getting things going with 0.5.7

--- model.js
+++ model.js
@@ -1,23 +1,24 @@
 Model = function(attributes) {
   this.attributes = attributes || {};
-  this.id = this.attributes._id;
   
-  if (this.id)
-    delete this.attributes._id;
+  // put _id on the main object as well as things like {{#each}} expect it to 
+  // be there. Also Meteor internals sometime set this without passing it into
+  // the constructor. So never assume it's in attributes.
+  this._id = this.attributes._id;
+  delete this.attributes._id;
   
   // begin with no errors
   this.errors = {};
 }
 
 Model.prototype = {
-  _meteorRawData: function() {
-    var data = _.extend({}, this.attributes);
-    data._id = this.id;
-    return data;
+  clone: function() {
+    // XXX: todo
+    return this;
   },
   
   persisted: function() {
-    return ('id' in this && this.id != null);
+    return ('_id' in this.attributes && this.attributes._id !== null);
   },
   
   save: function(update) {
@@ -52,4 +53,24 @@ Model.prototype = {
       this.constructor._collection.remove(this.id);
     }
   }
+}
+
+Model.extend = function(properties) {
+  // special named method 'initialize'
+  var init = function() {};
+  if (typeof properties.initialize === 'function') {
+    init = properties.initialize;
+    delete properties.initialize;
+  }
+    
+  var ctor = function(attrs) {
+    Model.call(this, attrs);
+    init();
+  }
+    
+  ctor.prototype = new Model();
+  _.extend(ctor.prototype, properties);
+  ctor.prototype.constructor = ctor;
+    
+  return ctor;
 }
